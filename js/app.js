2bcfe264eca4dc53ad0dec2c33036096a047d473
LaraPsc123 <larapascarella@gmail.com>
2024-07-19 23:59:35 +0200

    ADD: preso nome, cognome ed email dal dom

--- js/app.js
+++ js/app.js
@@ -5,9 +5,22 @@ console.log(formElement)
 // EVITARE L'EVENTO INVIO
 formElement.addEventListener('submit', function (event) {
     event.preventDefault()
-    // console.log('submit')
+    console.log('submit')
 })
 
+// PRENDO DAL DOM IL NOME
+const nomeElement = document.querySelector('input[type="nome"]')
+console.log(nomeElement)
+
+
+
+
+
+// PRENDO DAL DOM IL COGNOME
+const cognomeElement = document.getElementById('cognome')
+
+// PRENDO L'EMAIL
+const emailInputElement = document.getElementById('email')
 
 
 

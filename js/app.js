3bd6820af31e33f69f7534115d23039e38c8b5b2
LaraPsc123 <larapascarella@gmail.com>
2024-07-20 12:10:31 +0200

    ADD: recuperare gli id e il value della select

--- js/app.js
+++ js/app.js
@@ -1,26 +1,62 @@
+console.log('richiesta preventivo')
+
+// RECUPERARE L'INPUT DEL CLICK PER STAMPARE I VALORI DELLE CELLE
+const buttonCalcolaElement = document.getElementById('calcola')
+console.log(buttonCalcolaElement)
+// CREARE L'EVENTO QUANDO CLICCO SU CALCOLA
+buttonCalcolaElement.addEventListener('click', function () {
+    console.log('richiesta preventivo') // STAMPA DEL VALORE DEL NOME DATO DALL'UTENTE
+})
+
+
 // PRENDERE DAL DOM IL FORM
 const formElement = document.getElementById('form')
 console.log(formElement)
 
+const nomeElement = document.getElementById('nome')
+const cognomeElement = document.getElementById('cognome')
+const emailInputElement = document.getElementById('email')
+const tipoLavoroElement = document.getElementById("tipo-lavoro")
+const selectLavoroElement = document.getElementById("job")
+console.log(nomeElement, cognomeElement, emailInputElement, tipoLavoroElement)
+
 // EVITARE L'EVENTO INVIO
 formElement.addEventListener('submit', function (event) {
     event.preventDefault()
-    console.log('submit')
+    console.log('submit', event)
 })
 
-// PRENDO DAL DOM IL NOME
-const nomeElement = document.querySelector('input[type="nome"]')
-console.log(nomeElement)
+// RECUPERARE DALL'INPUT IL VALORE DEL NOME
+const nome = nomeElement.value
+console.log(nome)
+
+// RECUPERARE DALL'INPUT IL VALORE DEL COGNOME
+const cognome = cognomeElement.value
+console.log(cognome)
+
+// RECUPERARE IL VALORE DELLA SELECT
+const selectLavoro = selectLavoroElement.value
+console.log(selectLavoro)
+
+
+// AMMONTARE DEL PREVENTIVO PER LE ORE DI LAVORO RICHIESTE
+// PREZZO FINALE: NUM. ORE PER PREZZO ORARIO
+// const oreLavoro = 10 // number
+// const prezzoOrarioBackend = 20.50 // number
+// const prezzoOrarioFrontend = 15.30 // number
+// const prezzoOrarioProgettuale = 33.60 // number
+
+// // CALCOLO
+// let prezzoFinaleBackend = oreLavoro * prezzoOrarioBackend
+// let prezzoFinaleFrontend = oreLavoro * prezzoOrarioFrontend
+// let prezzoFinaleProgettuale = oreLavoro * prezzoOrarioProgettuale
+// console.log(prezzoFinaleBackend, prezzoFinaleFrontend, prezzoFinaleProgettuale)
 
 
 
 
 
-// PRENDO DAL DOM IL COGNOME
-const cognomeElement = document.getElementById('cognome')
 
-// PRENDO L'EMAIL
-const emailInputElement = document.getElementById('email')
 
 
 
